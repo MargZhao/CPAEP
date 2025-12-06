1a4f0ab42becf2faa2b0a25249c08695b8de5ce0
Ruichu Ao <r0927191@amazone.esat.kuleuven.be>
2025-12-06 17:24:48 +0100

    split task files between 2D and 444

--- includes/test_tasks.svh
+++ includes/test_tasks.svh
@@ -45,21 +45,3 @@ begin
 end
 endtask
 
-task automatic verify_444_result_c(
-  input logic signed [TileOutputDataWidth-1:0] golden_data [DataDepth],
-  input logic signed [TileOutputDataWidth-1:0] actual_data [DataDepth],
-  input logic        [          AddrWidth-1:0] num_data,
-  input logic                                  fatal_on_mismatch
-  );
-begin
-    // Compare with SRAM C contents
-  for (int unsigned addr = 0; addr < num_data; addr++) begin
-    if (golden_data[addr] !== actual_data[addr]) begin
-    $display("ERROR: Mismatch at address %0d: expected %d, got %d",
-            addr, golden_data[addr][OutDataWidth-1:0], actual_data[addr][OutDataWidth-1:0]);
-    $display("ERROR: Mismatch at address %0d: expected %b, got %b",
-            addr, golden_data[addr][OutDataWidth-1:0], actual_data[addr][OutDataWidth-1:0]);
-    if (fatal_on_mismatch)
-    $fatal;
-  end
-  endtask
--- tb/tb_444_mac_gemm.sv
+++ tb/tb_444_mac_gemm.sv
@@ -30,7 +30,7 @@ module tb_444_mac_gemm;
 
   // Test Parameters
   parameter int unsigned MaxNum   = 64;
-  parameter int unsigned NumTests = 10;
+  parameter int unsigned NumTests = 40;
 
   parameter int unsigned SingleM = 1;
   parameter int unsigned SingleK = 1;
@@ -184,7 +184,7 @@ module tb_444_mac_gemm;
   // Tasks and functions
   //---------------------------
   `include "includes/common_tasks.svh"
-  `include "includes/test_tasks.svh"
+  `include "includes/test_tasks_444.svh"
   `include "includes/test_func_444.svh"
 
   //---------------------------
@@ -235,19 +235,19 @@ module tb_444_mac_gemm;
 
       if (num_test == 0) begin
         M_i = 4;
-        K_i = 4;
-        N_i = 4;
-      end else if(num_test == 1) begin
-        M_i = 4;
-        K_i = 5;
-        N_i = 32;
-      end else if(num_test == 2) begin
-        M_i = 4;
         K_i = 64;
-        N_i = 1;
+        N_i = 16;
+      end else if(num_test == 1) begin
+        M_i = 16;
+        K_i = 64;
+        N_i = 4;
+      end else if(num_test == 2) begin
+        M_i = 32;
+        K_i = 32;
+        N_i = 32;
        end else if(num_test == 3) begin
         M_i = 1;
-        K_i = 4;
+        K_i = 1;
         N_i = 1;
       end else begin
         // Randomize matrix sizes
@@ -324,9 +324,9 @@ module tb_444_mac_gemm;
               global_m = m_tile * RowsPerTile + row;
               global_k = k * NumInputs + i;
               if ((global_m < M_i) && (global_k < K_i)) begin
-                  //val = $urandom() % (2 ** InDataWidth);
+                  val = $urandom() % (2 ** InDataWidth);
                   //val = (global_m*64 + global_k) % (2 ** (InDataWidth-1));
-                  val = global_k; // For easier debugging
+                  //val = global_k; // For easier debugging
                  
                   temp_row_pack_data[row*NumInputs*InDataWidth + i*InDataWidth +: InDataWidth] = val;
               end
@@ -346,9 +346,9 @@ module tb_444_mac_gemm;
               global_n = n_tile * ColsPerTile + col;
               global_k = k * NumInputs + i;
               if ((global_n < N_i) && (global_k < K_i)) begin
-                  //val = $urandom() % (2 ** InDataWidth);
+                  val = $urandom() % (2 ** InDataWidth);
                   //val = (global_n*64 + global_k) % (2 ** (InDataWidth-1));
-                  val = global_k; // For easier debugging
+                  //val = global_k; // For easier debugging
                  
                   temp_col_pack_data[col*NumInputs*InDataWidth + i*InDataWidth +: InDataWidth] = val;
               end
